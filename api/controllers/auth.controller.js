c66e76bbd3c950bcb08e8eccee92c2a41c803e80
venkatesh-ididala <venkateshididala@gmail.com>
2024-09-15 10:39:28 +0530

    create signup route

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,24 @@
+import User from '../models/user.model.js'
+import bcryptjs from 'bcryptjs';
+
+
+export const signup=async(req,res)=>{
+
+    const {username,password,email}=req.body;
+
+    const hashedPassword=bcryptjs.hashSync(password,10);
+    const newUser=new User({username,password:hashedPassword,email});
+
+    try{
+        await newUser.save();
+
+        res.status(201).json({
+            status:'success',
+            message:'user created successfully'
+        });
+    }catch(err){
+        res.status(500).json(err.message);
+    }
+    
+}
+
--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js'
+import authRouter from './routes/auth.route.js'
 
 
 dotenv.config();
@@ -18,9 +19,13 @@ app.listen(3000,()=>{
     console.log('server is running on port 3000');
 });
 
+//allows json data input 
+app.use(express.json());
+
 
 
 app.use("/api/user",userRouter);
+app.use('/api/auth',authRouter);
 
 
 // app.get('/test',(req,res)=>{
