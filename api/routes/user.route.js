6a694de65cd47d4d0d60d4c6a3f5adcfcd4bdd97
venkatesh-ididala <venkateshididala@gmail.com>
2024-09-16 21:05:11 +0530

    add contact component and get landlord

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,12 +1,14 @@
 import express from 'express';
-import { test ,updateuser,deleteuser} from '../controllers/user.controller.js'
+import { deleteUser, test, updateUser,  getUserListings, getUser} from '../controllers/user.controller.js';
 import { verifyToken } from '../utils/verifyuser.js';
 
-const router=express.Router();
 
+const router = express.Router();
 
-router.get('/test',test)
-router.post('/update/:id',verifyToken,updateuser);
-router.delete('/delete/:id',verifyToken,deleteuser);
+router.get('/test', test);
+router.post('/update/:id', verifyToken, updateUser)
+router.delete('/delete/:id', verifyToken, deleteUser)
+router.get('/listings/:id', verifyToken, getUserListings)
+router.get('/:id', verifyToken, getUser)
 
 export default router;
