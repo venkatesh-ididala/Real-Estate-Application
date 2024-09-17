d047c9f8d5824f99fae8f18d4bbc00090f8a5fc1
venkatesh-ididala <venkateshididala@gmail.com>
2024-09-17 11:55:05 +0530

    add preinstallation

--- api/index.js
+++ api/index.js
@@ -4,17 +4,25 @@ import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js'
 import authRouter from './routes/auth.route.js'
 import cookieParser from 'cookie-parser';
+import path from 'path';
 import listingRouter from  './routes/listing.route.js';
 
 dotenv.config();
-const app=express();
+
+
+
 
 mongoose.connect(process.env.MONGO).then(()=>{
     console.log("connected to database");
 }).catch((err)=>{
     console.log(err);
-})
+});
 
+//dynamic directory name 
+const __dirname=path.resolve();
+
+
+const app=express();
 
 app.listen(3000,()=>{
     console.log('server is running on port 3000');
@@ -30,6 +38,15 @@ app.use(express.json());
 app.use("/api/user",userRouter);
 app.use('/api/auth',authRouter);
 app.use('/api/listing',listingRouter);
+
+
+app.use(express.static(path.join(__dirname,'/client/dist')));
+
+app.get('*',(req,res)=>{
+   
+    res.sendFile(path.join(__dirname,'client','dist','index.html'))
+})
+
 
 
 app.use((err,req,res,next)=>{
